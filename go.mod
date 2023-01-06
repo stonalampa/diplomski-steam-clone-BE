77959782405f441d19537807223d457905774490
Milan <milan.radanovic@sprintingsoftware.com>
2023-01-06 15:04:27 +0100

    Configured viper and cobra, added db connection

--- go.mod
+++ go.mod
@@ -2,23 +2,23 @@ module main
 
 go 1.19
 
-require go.mongodb.org/mongo-driver v1.11.1
+require (
+	github.com/gin-gonic/gin v1.8.2
+	github.com/spf13/cobra v1.6.1
+	github.com/spf13/viper v1.14.0
+	go.mongodb.org/mongo-driver v1.11.1
+)
 
 require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fsnotify/fsnotify v1.6.0 // indirect
-	github.com/gin-contrib/cors v1.4.0 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
-	github.com/gin-gonic/gin v1.8.2 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.11.1 // indirect
 	github.com/goccy/go-json v0.10.0 // indirect
-	github.com/golang-jwt/jwt/v4 v4.4.3 // indirect
 	github.com/golang/snappy v0.0.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
-	github.com/inconshreveable/mousetrap v1.1.0 // indirect
-	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
@@ -31,14 +31,10 @@ require (
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.6 // indirect
 	github.com/pkg/errors v0.9.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/spf13/afero v1.9.2 // indirect
 	github.com/spf13/cast v1.5.0 // indirect
-	github.com/spf13/cobra v1.6.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.14.0 // indirect
-	github.com/stretchr/testify v1.8.1 // indirect
 	github.com/subosito/gotenv v1.4.1 // indirect
 	github.com/ugorji/go/codec v1.2.8 // indirect
 	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
--- main.go
+++ main.go
@@ -1,57 +1,118 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"log"
 	"os"
+	"time"
 
-	envs "main/env"
-
-	"github.com/gin-gonic/gin"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func main() {
-	var shouldSeed string
-	var config []string
-	env := os.Args[1]
-	if len(os.Args) > 2 {
-		shouldSeed = os.Args[2]
+var (
+	env     string
+	seed    string
+	rootCmd = &cobra.Command{
+		Use:       "seed",
+		Short:     "If TRUE it will run seeding.",
+		Long:      "If TRUE the seeding will be done, if FALSE the program will run normally.",
+		ValidArgs: []string{"true", "false", "local", "deployment"},
+		Args:      matchAll(cobra.MinimumNArgs(2), cobra.OnlyValidArgs),
+		Run: func(cmd *cobra.Command, args []string) {
+			if args[0] == "true" || args[0] == "false" {
+				env = args[1]
+				seed = args[0]
+			} else {
+				env = args[0]
+				seed = args[1]
+			}
+		},
 	}
-	if env != "local" {
-		config = envs.EnvConfig("deployedConfig")
-	} else {
-		config = envs.EnvConfig("localConfig")
+)
+
+func matchAll(checks ...cobra.PositionalArgs) cobra.PositionalArgs {
+	return func(cmd *cobra.Command, args []string) error {
+		for _, check := range checks {
+			if err := check(cmd, args); err != nil {
+				return err
+			}
+		}
+		return nil
+	}
+}
+
+func main() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
+	//* Set configuration
+	configLocation := "./env/" + env + ".yaml"
+	viper.SetConfigFile(configLocation)
+	viper.Set("env", env)
+	viper.Set("seed", seed)
+	err := viper.ReadInConfig()
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
+	// * Connect to db, create repository
+	var dbCredentials = options.Credential{
+		AuthMechanism: "SCRAM-SHA-1",
+		AuthSource:    viper.GetString("name"),
+		Username:      viper.GetString("username"),
+		Password:      viper.GetString("password"),
+		PasswordSet:   true,
+	}
+	// create a database connection
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI(viper.GetString("connectionString")).SetAuth((dbCredentials)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	if err := client.Connect(context.TODO()); err != nil {
+		log.Fatal(err)
+	}
+
+	// repository := repository.NewRepository(client.Database(viper.GetString("name")))
+
+	// * Connect to server
 	// client := database.DatabaseConnector(config)
 	// defer client.Disconnect(context.Background())
 
-	if shouldSeed == "seed" {
-		// ctx := context.TODO()
-		// fmt.Print("OVDE JE@@\n")
-		// database.Seeder(env, client, ctx)
-	} else {
-		fmt.Print(config)
-		r := gin.Default()
-		r.GET("", func(c *gin.Context) {
-			c.String(200, "Welcome to Go and Gin!")
-		})
-		r.Run(":3030")
-		// if env != "local" {
-		// 	config = envs.EnvConfig("deployedConfig")
-		// } else {
-		// 	config = envs.EnvConfig("localConfig")
-		// }
+	// if shouldSeed == "seed" {
+	// 	// ctx := context.TODO()
+	// 	// fmt.Print("OVDE JE@@\n")
+	// 	// database.Seeder(env, client, ctx)
+	// } else {
+	// 	fmt.Print(config)
+	// 	r := gin.Default()
+	// 	r.GET("", func(c *gin.Context) {
+	// 		c.String(200, "Welcome to Go and Gin!")
+	// 	})
+	// 	r.Run(":3030")
+	// if env != "local" {
+	// 	config = envs.EnvConfig("deployedConfig")
+	// } else {
+	// 	config = envs.EnvConfig("localConfig")
+	// }
 
-		// var db *mongo.Database
-		// var ctx context.Context
-		// var cancel context.CancelFunc
-		// var client *mongo
-		//initialize database and context
-		// client := database.DatabaseConnector(config)
-		// defer cancel()
-		// client = dataLayer.InitDataLayer()
-		// defer client.Disconnect(context.Background())
-		// fmt.Println(db, ctx)
-	}
+	// var db *mongo.Database
+	// var ctx context.Context
+	// var cancel context.CancelFunc
+	// var client *mongo
+	//initialize database and context
+	// client := database.DatabaseConnector(config)
+	// defer cancel()
+	// client = dataLayer.InitDataLayer()
+	// defer client.Disconnect(context.Background())
+	// fmt.Println(db, ctx)
+	// }
 
 }
